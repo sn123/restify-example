c9d041a242cbc38966c9f4896680c7cc324024cd
Sachin Nigam <snigam@goavega.com>
2019-07-27 11:44:46 +0530

    Added new controller

--- src/app/controllers/index.ts
+++ src/app/controllers/index.ts
@@ -1,1 +1,2 @@
 export { UserController } from './user-controller';
+export { CampaignController } from './camgaign-controller';
--- src/bootstrap/controllers.ts
+++ src/bootstrap/controllers.ts
@@ -1,9 +1,17 @@
 import { Container } from 'inversify';
 import { interfaces, TYPE } from 'inversify-restify-utils';
-import { UserController } from '../app/controllers';
+import { UserController, CampaignController } from '../app/controllers';
 
 export class Controllers {
     public static bootstrap(container: Container): void {
-        container.bind<interfaces.Controller>(TYPE.Controller).to(UserController);
+        // TODO: use Symbol.For in config
+        container
+            .bind<interfaces.Controller>(TYPE.Controller)
+            .to(UserController)
+            .whenTargetNamed('UserController');
+        container
+            .bind<interfaces.Controller>(TYPE.Controller)
+            .to(CampaignController)
+            .whenTargetNamed('CampaignController');
     }
 }
